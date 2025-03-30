04e6b97b9df8c958f45bdf1f50d89b47d85eb2ec
Nam Pham <pknam25@gmail.com>
2025-03-30 20:13:56 +0800

    Dockerised front-end app

--- demo_app/front-end/src/services/quotes-service.ts
+++ demo_app/front-end/src/services/quotes-service.ts
@@ -7,7 +7,7 @@ export const QuoteService = {
   // Get a random quote
   getRandomQuote: async (): Promise<Quote> => {
     try {
-      const response = await axios.get<Quote>(`${QUOTABLE_API_URL}/random`)
+      const response = await axios.get<{ quote: Quote }>(`${QUOTABLE_API_URL}/random`)
       return response.data.quote
     } catch (error) {
       console.error('Error fetching random quote:', error)
