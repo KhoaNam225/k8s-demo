6b5a913c71f1209b43b8060db9975120e1375c91
Nam Pham <pknam25@gmail.com>
2025-03-30 19:10:02 +0800

    Fetched a random quote in FastAPI back-end

--- demo_app/back-end/main.py
+++ demo_app/back-end/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from httpx import AsyncClient
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/api/v1/quotes/random")
+async def random_quote():
+    random_quote_api_base_url = "https://api.quotable.kurokeita.dev/api/quotes/random"
+
+    async with AsyncClient() as client:
+        response = await client.get(random_quote_api_base_url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return {"error": "Failed to fetch quote"}
