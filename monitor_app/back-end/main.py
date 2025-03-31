4b5d9aec2b59c38fa91cf91cbef6029bcfe12881
Nam Pham <pknam25@gmail.com>
2025-03-31 14:38:29 +0800

    Init the back-end for monitoring app

---
+++ monitor_app/back-end/main.py
@@ -0,0 +1,30 @@
+from kubernetes import client, config
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+# Initialize FastAPI app
+app = FastAPI()
+
+# Allow CORS for all origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+# Configs can be set in Configuration class directly or using helper utility
+# config.load_kube_config()
+
+# v1 = client.CoreV1Api()
+# print("Listing pods with their IPs:")
+# ret = v1.list_pod_for_all_namespaces(watch=False)
+# for i in ret.items:
+#     print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
